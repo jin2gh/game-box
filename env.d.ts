322a49fe0533d191ca675b95932b461abf7ca010
jin <jinjiting2690@gmail.com>
2023-02-24 16:29:42 +0800

    chore(*): update vite config file

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+
+declare module 'rollup-plugin-external-globals'
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,44 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import importToCDN from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/game-box',
-  plugins: [vue(), vueJsx()],
+  base: '/game-box/',
+  // build: {
+  //   rollupOptions: {
+  //     external: ['vue','vue-router'],
+  //     plugins: [
+  //       externalGlobals({
+  //         vue: 'Vue',
+  //         'vue-router': 'VueRouter'
+  //       })
+  //     ]
+  //   }
+  // },
+  plugins: [
+    vue(),
+    vueJsx(),
+    importToCDN({
+      modules: [
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: `https://cdn.jsdelivr.net/npm/vue@3.2.38/dist/vue.global.min.js`,
+        },
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: `https://cdn.jsdelivr.net/npm/vue-router@4.1.5/dist/vue-router.global.min.js`,
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  }
+  },
 })
